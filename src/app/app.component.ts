b0598af0ea13e5159aa93ee1e30c13db91b028d1
ThibaudBOURGEAT <thibaud.bourgeat.trx@gmail.com>
2018-09-27 15:13:10 +0200

    v2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, ViewChild} from '@angular/core';
 import {NavController, Platform} from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { Push, PushObject, PushOptions } from '@ionic-native/push';
 
 import { MenuPage } from '../pages/menu/menu';
 
@@ -12,13 +13,34 @@ export class MyApp {
   @ViewChild('myNav') nav: NavController
   rootPage:any = MenuPage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, private push: Push) {
     platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
+      this.pushStetup();
     });
+  }
+
+  pushStetup(){
+    const options: PushOptions = {
+       android: {
+         senderID: '37033292368'
+       },
+       ios: {
+           alert: 'true',
+           badge: true,
+           sound: 'false'
+       }
+   };
+
+    const pushObject: PushObject = this.push.init(options);
+
+
+    pushObject.on('notification').subscribe((notification: any) => console.log('Vous avez reçu une notification', notification));
+
+    pushObject.on('registration').subscribe((registration: any) => console.log('Appareil enregistré', registration));
+
+    pushObject.on('error').subscribe(error => console.error('Erreur avec le plugin Push', error));
   }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,8 @@ import { TopicalityProvider } from '../providers/topicality/topicality';
 import { DatePipe } from '@angular/common';
 import { EmailComposer } from '@ionic-native/email-composer';
 
+import { Push } from '@ionic-native/push';
+
 import { MyApp } from './app.component';
 import { WhoAreWePage } from '../pages/who-are-we/who-are-we';
 import { MenuPage } from '../pages/menu/menu';
@@ -19,20 +21,6 @@ import { ComiteComponent } from '../components/comite/comite';
 import { PartenaireComponent } from '../components/partenaire/partenaire';
 import { ContactPage } from '../pages/contact/contact';
 import { IonicImageViewerModule } from 'ionic-img-viewer';
-
-import { Firebase } from '@ionic-native/firebase';
-import { FcmProvider } from '../providers/fcm/fcm';
-import { AngularFireModule } from '@angular/fire';
-import { AngularFirestoreModule } from 'angularfire2/firestore';
-
-const firebase = {
-  apiKey: "AIzaSyALSMmT2f1UTPsctKCyQDsvwmlVmGjBzCs",
-  authDomain: "aer-cong.firebaseapp.com",
-  databaseURL: "https://aer-cong.firebaseio.com/",
-  projectId: "aer-cong",
-  storageBucket: "aer-cong.appspot.com",
-  messagingSenderId: "37033292368"
-}
 
 @NgModule({
   declarations: [
@@ -50,8 +38,6 @@ const firebase = {
     BrowserModule,
     HttpClientModule,
     IonicModule.forRoot(MyApp),
-    AngularFireModule.initializeApp(firebase),
-    AngularFirestoreModule,
     IonicImageViewerModule
   ],
   bootstrap: [IonicApp],
@@ -72,9 +58,8 @@ const firebase = {
     TopicalityProvider,
     DatePipe,
     EmailComposer,
+    Push,
     {provide: ErrorHandler, useClass: IonicErrorHandler},
-    FcmProvider,
-    Firebase
   ]
 })
 export class AppModule {}
